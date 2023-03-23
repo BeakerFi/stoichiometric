70c265b5faba13eef4ea8892d6a2a595eece8ee7
avincigu <arthur.vinciguerra@ens-lyon.fr>
2023-03-23 18:41:28 +0100

    new addresses

--- frontend/src/utils/general/constants.tsx
+++ frontend/src/utils/general/constants.tsx
@@ -15,31 +15,33 @@ const backend_api_url = 'https://beaker.fi:8888'
     Dao constants
  */
 
-const dao_address = "";
+const dao_address = "component_tdx_b_1qfge7gx8kw86jd34xvsa9lp6s3jv98jt6e7lnks983jq2n7346";
 
-const voter_card_address = "";
+const voter_card_address = "resource_tdx_b_1qpge7gx8kw86jd34xvsa9lp6s3jv98jt6e7lnks983jqqjvlsp";
 
-const proposal_receipt_address = "";
+const proposal_receipt_address = "resource_tdx_b_1qqqujxnr6vt2gxkj6acge3qmp6wzv7lf32xx3dze86xqa7fvpl";
 
 
 /*
     Dex constants
  */
 
-const router_address = "component_tdx_b_1q2ynsv6lj92vgr5n2kef79jjhrad3x766m72lvvgjteqq9c60n";
+const router_address = "component_tdx_b_1q2f2qy0qv5t2r4rx9jag5p6shms8es57e7wqjc7cjgnqmh62fg";
 
-const position_address = "resource_tdx_b_1qp407a3xpy5l9gzzx8kktfgcs3asnsm3u4hglq2ayn7qf9qpyt";
+const position_address = "resource_tdx_b_1qru8j38866335wdr7xwj09u95rwllscrqkfcvk8pzfwslvav9g";
 
 
 /*
     Stablecoin constants
  */
 
-const issuer_address = "component_tdx_b_1qfhyydhm77z6y82x36ywkjgwsaqaev5f2z3hszpfcy6q6lclud";
+const issuer_address = "component_tdx_b_1qtnssm8g0zwev20ynn7578l3parucku6x6x7nudj7jzqf6xvsu";
 
-const stablecoin_address = "resource_tdx_b_1qpua4ltl0s8yh7yqvwysqnulvka7jkngyafaw57zd9dqatp3hj";
+const stablecoin_address = "resource_tdx_b_1qzs2upr69pc4djesgpl864ajmhqsmf58jxqsv5ftuphsc4gt7k";
 
-const loan_address = "resource_tdx_b_1qqwunyhu96jtwpwjwup3jndmws0xht3ul3ncqa2r23cqghtj6j";
+const loan_address = "resource_tdx_b_1qr2g063eurussjcajx3585684tz8m0lszzz298dh0lqshms03l";
+
+const flash_mint_address = "resource_tdx_b_1qq0k3ggft9demr2k874xta398dj7j0p984efu3wl08dsx0grle";
 
 const stable_coin = {name: "Stoichiometric USD", symb: "SUSD", address: stablecoin_address, icon_url: "https://cdn-icons-png.flaticon.com/512/3215/3215346.png"}
 
@@ -49,4 +51,4 @@ const token_default = {name: 'Wrapped Bitcoin', symb: 'WBTC', address: 'resource
 
 
 
-export {dAppId, radix_api_url, backend_api_url, dao_address, voter_card_address, proposal_receipt_address, router_address, position_address, issuer_address, stablecoin_address, loan_address, stable_coin, token_default}
+export {dAppId, radix_api_url, backend_api_url, dao_address, voter_card_address, proposal_receipt_address, router_address, position_address, issuer_address, stablecoin_address, loan_address, flash_mint_address, stable_coin, token_default}
